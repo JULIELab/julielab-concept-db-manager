a79cacb20871c906ada0c7e8c9caba46e7ece7a1
khituras <chew@gmx.net>
2020-05-12 09:56:01 +0200

    Concepts are now imported in batches of 50. This is a measure to reduce memory consumption and aims at making larger imports possible.

--- julielab-concept-db-manager-core/src/main/java/de/julielab/concepts/db/core/ServerPluginConceptInserter.java
+++ julielab-concept-db-manager-core/src/main/java/de/julielab/concepts/db/core/ServerPluginConceptInserter.java
@@ -14,6 +14,7 @@ import de.julielab.java.utilities.ConfigurationUtilities;
 import de.julielab.neo4j.plugins.ConceptManager;
 import de.julielab.neo4j.plugins.datarepresentation.ImportConcept;
 import de.julielab.neo4j.plugins.datarepresentation.ImportConcepts;
+import de.julielab.neo4j.plugins.datarepresentation.ImportFacet;
 import org.apache.commons.configuration2.ConfigurationUtils;
 import org.apache.commons.configuration2.HierarchicalConfiguration;
 import org.apache.commons.configuration2.ex.ConfigurationException;
@@ -35,7 +36,7 @@ import static de.julielab.java.utilities.ConfigurationUtilities.checkParameters;
 import static de.julielab.java.utilities.ConfigurationUtilities.slash;
 
 public class ServerPluginConceptInserter implements ConceptInserter {
-
+    public static final int CONCEPT_IMPORT_BATCH_SIZE = 50;
     private static final Logger log = LoggerFactory.getLogger(ServerPluginConceptInserter.class);
     private HierarchicalConfiguration<ImmutableNode> connectionConfiguration;
 
@@ -55,28 +56,41 @@ public class ServerPluginConceptInserter implements ConceptInserter {
             HttpConnectionService httpService = HttpConnectionService.getInstance();
             HttpPost httpPost = httpService.getHttpPostRequest(connectionConfiguration, serverUri + String
                     .format(ServerPluginConnectionConstants.SERVER_PLUGIN_PATH_FMT, pluginName, pluginEndpoint));
+
+            // We first create the facet. While this can be done in a single request together with the concepts
+            // to be imported, we want to import the concepts batch-wise. This requires knowledge of the target
+            // facet ID. Because when we don't specify the facet ID, a new facet would be created with each
+            // batch of concepts.
+            String facetId = createFacet(concepts, jsonMapper, httpPost);
+
+            ImportFacet createdFacet = new ImportFacet(facetId);
+            String createdFacetJson = jsonMapper.writeValueAsString(createdFacet);
             Map<String, String> dataMap = new HashMap<>();
             StringWriter sw = new StringWriter();
             List<ImportConcept> importConcepts = concepts.getConcepts();
+            sw.write("[");
             for (int i = 0; i < importConcepts.size(); i++) {
                 ImportConcept importConcept = importConcepts.get(i);
-                try {
-                    jsonMapper.writeValue(sw, importConcept);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                jsonMapper.writeValue(sw, importConcept);
+                if (i > 0 && (i % CONCEPT_IMPORT_BATCH_SIZE == 0 || i == importConcepts.size() - 1)) {
+                    sw.write("]");
+                    dataMap.put(ConceptManager.KEY_CONCEPTS, sw.getBuffer().toString());
+                    dataMap.put(ConceptManager.KEY_FACET, createdFacetJson);
+                    if (concepts.getImportOptions() != null)
+                        dataMap.put(ConceptManager.KEY_IMPORT_OPTIONS,
+                                jsonMapper.writeValueAsString(concepts.getImportOptions()));
+                    httpPost.setEntity(new StringEntity(jsonMapper.writeValueAsString(dataMap)));
+
+                    String response = HttpConnectionService.getInstance().sendRequest(httpPost);
+                    if (log.isDebugEnabled())
+                        log.debug("Server plugin response to concept insertion for batch {}: {}", response, (int) (i / CONCEPT_IMPORT_BATCH_SIZE));
+                    sw.getBuffer().delete(0, sw.getBuffer().length());
+                    sw.write("[");
+                } else {
+                    sw.write(",");
                 }
-                importConcepts.set(i, null);
             }
             //dataMap.put(ConceptManager.KEY_CONCEPTS, jsonMapper.writeValueAsString(concepts.getConcepts()));
-            dataMap.put(ConceptManager.KEY_CONCEPTS, sw.getBuffer().toString());
-            dataMap.put(ConceptManager.KEY_FACET, jsonMapper.writeValueAsString(concepts.getFacet()));
-            if (concepts.getImportOptions() != null)
-                dataMap.put(ConceptManager.KEY_IMPORT_OPTIONS,
-                        jsonMapper.writeValueAsString(concepts.getImportOptions()));
-            httpPost.setEntity(new StringEntity(jsonMapper.writeValueAsString(dataMap)));
-
-            String response = HttpConnectionService.getInstance().sendRequest(httpPost);
-            log.debug("Server plugin response to concept insertion: {}", response);
         } catch (InternalNeo4jException e) {
             final ObjectMapper om = new ObjectMapper();
             om.enable(SerializationFeature.INDENT_OUTPUT);
@@ -87,13 +101,26 @@ public class ServerPluginConceptInserter implements ConceptInserter {
             }
             throw new ConceptInsertionException(e.getErrorResponse().getMessage());
         } catch
-                (ConceptDatabaseConnectionException | JsonProcessingException | UnsupportedEncodingException
+        (ConceptDatabaseConnectionException | JsonProcessingException | UnsupportedEncodingException
                         e) {
             throw new ConceptInsertionException(e);
         } catch (ConfigurationException e) {
             log.error("Configuration error occured with configuration {} {}", ConfigurationUtilities.LS, ConfigurationUtils.toString(importConfig));
             throw new ConceptInsertionException(e);
+        } catch (IOException e) {
+            log.error("Could not convert (parts of) ImportConcepts to JSON", e);
+            throw new ConceptInsertionException(e);
         }
+    }
+
+    private String createFacet(ImportConcepts concepts, ObjectMapper jsonMapper, HttpPost httpPost) throws ConceptDatabaseConnectionException, IOException {
+        Map<String, String> facetDataMap = new HashMap<>();
+        facetDataMap.put(ConceptManager.KEY_FACET, jsonMapper.writeValueAsString(concepts.getFacet()));
+        httpPost.setEntity(new StringEntity(jsonMapper.writeValueAsString(facetDataMap)));
+        String facetCreationResponse = HttpConnectionService.getInstance().sendRequest(httpPost);
+        log.debug("Server plugin response to facet creation: {}", facetCreationResponse);
+        Map<?, ?> response = jsonMapper.readValue(facetCreationResponse, Map.class);
+        return (String) response.get(ConceptManager.KEY_FACET_ID);
     }
 
     @Override
